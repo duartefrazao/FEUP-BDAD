19a47ababb93a2bc99bcc47226ae31586b9e1f32
Diogo Yaguas <diogo.yaguas@hotmail.com>
2018-05-25 11:49:57 +0100

    Added query 4 and 5

--- int4.sql
+++ int4.sql
@@ -1,3 +1,14 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+-- Quais os três condutores com melhor pontuação
+
+SELECT
+ nome,
+ pontuaçao_condutor
+FROM
+ condutores
+ORDER BY
+ pontuaçao_condutor
+LIMIT 3;
