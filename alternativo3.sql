7bc2af01703c034b2718517de364e248050a43b2
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-05-27 22:59:27 +0100

    Alternativo int7

--- alternativo3.sql
+++ alternativo3.sql
@@ -1,23 +1,4 @@
-.mode columns
-.header on
-.nullvalue NULL
+create view hadFive as 
+select utilizador from Avaliacao where avaliacao_passageiro = 5;
 
-PRAGMA foreign_keys = ON;
-
--- Impede que um utilizador se associe a uma viagem em que ele próprio é o condutor
-
-CREATE TRIGGER ImpedirAssociar
-BEFORE INSERT ON LocalParagem
-BEGIN
-    SELECT RAISE(abort,'Utilizador é o condutor! Impossível associar')
-    where exists(
-    select * from Viagem join Partilha 
-        where new.viagem=Viagem.id AND
-        Viagem.partilha_associada = Partilha.id AND
-        Partilha.condutor = new.utilizador
-);
-
-END;
-
-
-
+select numero_up from Utilizador where numero_up not in(select * from hadFive);
