c5a10e026eb81f95aa15a98ca3c17145d358f5cf
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-05-27 22:46:51 +0100

    Alternativo 3

---
+++ alternativo3.sql
@@ -0,0 +1,23 @@
+.mode columns
+.header on
+.nullvalue NULL
+
+PRAGMA foreign_keys = ON;
+
+-- Impede que um utilizador se associe a uma viagem em que ele próprio é o condutor
+
+CREATE TRIGGER ImpedirAssociar
+BEFORE INSERT ON LocalParagem
+BEGIN
+    SELECT RAISE(abort,'Utilizador é o condutor! Impossível associar')
+    where exists(
+    select * from Viagem join Partilha 
+        where new.viagem=Viagem.id AND
+        Viagem.partilha_associada = Partilha.id AND
+        Partilha.condutor = new.utilizador
+);
+
+END;
+
+
+
