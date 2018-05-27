e557657152fb8a4dea962234ce9d47d55a4cd902
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-05-27 23:25:15 +0100

    Alternativo 8

--- alternativo3.sql
+++ alternativo3.sql
@@ -1,4 +1,11 @@
+----7
 create view hadFive as 
 select utilizador from Avaliacao where avaliacao_passageiro = 5;
 
 select numero_up from Utilizador where numero_up not in(select * from hadFive);
+
+
+---8
+select Viagem.id as ViagemID, lugares_disponiveis
+from Viagem join Zona join Partilha join LocalParagem
+where zona.id=LocalParagem.zona and LocalParagem.viagem=Viagem.id and Viagem.partilha_associada = Partilha.id and data="2018-03-06" and Zona.nome = 'Amial' and hora_começo > '10:20 AM' AND hora_começo < '3:32 PM';
