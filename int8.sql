4b191fb348dd12810a7e64508563b64f30b784d6
Henrique Gonçalves <36671768+up201608320@users.noreply.github.com>
2018-05-27 21:45:36 +0100

    Update int8.sql

--- int8.sql
+++ int8.sql
@@ -1,3 +1,12 @@
-.mode columns
-.headers on
-.nullvalue NULL
+-- Viagens para o dia x num certo intervalo de horas que passam no sítio que quero
+
+select*
+
+from viagem
+
+where data="2017-11-23" and id in(select id
+					from partilha
+					where hora_começo > '10:20 AM' AND hora_começo < '3:32 PM')
+			and id in(select viagem
+					from LocalidadesPossiveis
+					where zona = "1")
