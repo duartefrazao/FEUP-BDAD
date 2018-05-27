893912c8b19b0e9cf9da6b54709a8d2c07fa0185
Henrique Gonçalves <36671768+up201608320@users.noreply.github.com>
2018-05-27 23:16:32 +0100

    Update int9.sql

--- int9.sql
+++ int9.sql
@@ -1,3 +1,19 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+--Qual o condutor com o qual já fiz mais viagens?
+
+SELECT condutor,utilizador,
+       Max(n) "Vezes com que utilizador viajou"
+FROM   (SELECT condutor,utilizador,
+               Count(*) n
+        FROM   (SELECT partilha.id,utilizador,
+                       condutor
+                FROM   partilha,
+                       viagem,
+		       localParagem
+                WHERE  localParagem.viagem = viagem.id
+                       AND partilha_associada = partilha.id
+		       AND utilizador = "201620178")
+        GROUP  BY condutor);
