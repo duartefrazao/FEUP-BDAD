4dfd73bd9d3cc1a6e57f4dc5df674659125f3844
Henrique Gonçalves <36671768+up201608320@users.noreply.github.com>
2018-05-27 21:46:15 +0100

    Update int10.sql

--- int10.sql
+++ int10.sql
@@ -1,3 +1,13 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+-- Pares de viagens diários que me levam e trazem para a FEUP num certo intervalo de horas?
+
+select*
+
+from viagem
+
+where id in(select id
+		from partilha
+		where hora_começo > '10:20 AM' AND hora_começo < '3:32 PM')
